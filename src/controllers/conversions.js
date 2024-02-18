4bb0e29e0d49958a1aedc45a1b59f5b6224f3782
raghava219 <raghava219@gmail.com>
2024-02-18 22:18:31 +0530

    Controller will return the PDF generated by html-to-pdf.

--- src/controllers/conversions.js
+++ src/controllers/conversions.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import htmlToPDF from '../helpers/html-to-pdf.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("Conversion controller");
+router.get('/', async (req, res) => {
+
+    const pdf = await htmlToPDF('<html><body><h1>Conversion Controller in PDF</h1></body></html>');
+    res.contentType('application/pdf');
+    res.send(pdf);
+
 });
 
 export default router;
